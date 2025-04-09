665fb00903664661d71e21cdef36179e5f42fa28
kostasntnt <kostasntnt@hotmail.gr>
2025-04-09 18:52:43 +0300

    9/4 neo commit

--- TouristGuide/src/app/layouts/second-layout/login/login.component.ts
+++ TouristGuide/src/app/layouts/second-layout/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../../services/login.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  user ={
+    username:'',
+    password:''
+  }  
+
+  constructor(private loginService: LoginService, private router: Router) {}
+
+  onLogin() {
+    this.loginService.login(this.user).subscribe(
+      response => {
+        console.log('Σύνδεση επιτυχής:', response);
+        alert('Καλωσήρθες, ' + response.username);
+        this.router.navigate(['/userhome']);
+      },
+      error => {
+        console.error('Σφάλμα σύνδεσης:', error);
+        alert('Λάθος στοιχεία σύνδεσης!');
+      }
+    );
+  } 
 
 }
--- TouristPy/app/routes.py
+++ TouristPy/app/routes.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, request, jsonify 
 from app import db 
 from flask_cors import cross_origin
+import bcrypt
+from flask import session
 
 routes = Blueprint('routes', __name__)
 
+@cross_origin(origins='http://localhost:4200', supports_credentials=True)
+
 @routes.route('/signup', methods=['POST'])
-@cross_origin(origin='http://localhost:4200', supports_credentials=True)
 def signup():
     data =request.json
 
@@ -18,15 +21,41 @@ def signup():
 
     if not firstname or not lastname or not email or not username or not password:
         return jsonify({"error": "Missing fields"}), 400
+    
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     db.users.insert_one({
         "firstname": firstname,
         "lastname": lastname,
         "email": email,
         "username": username,
-        "password": password
+        "password": hashed_password.decode('utf-8')
 
     })
 
     print("Λήφθηκε αίτημα εγγραφής:", request.json)
     return jsonify({"message": "User created successfully"}), 201
+
+
+@routes.route('/login',methods=['POST'] )
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or not password:
+        return jsonify({"error: Συμπληρώστε τα πεδία"}), 400
+    
+    user =db.users.find_one({"username": username})
+
+    if not user:
+        return jsonify({"error":"Λάθος όνομα χρήστη ή κωδικού"}),401
+    
+    if not bcrypt.checkpw(password.encode('utf-8'), user["password"].encode('utf-8')):
+        return jsonify({"error": "Λάθος όνομα χρήστη ή κωδικού"}),401
+
+    return jsonify({"message": "Η σύνδεση ολοκληρώθηκε επιτυχώς", "username": user["username"]}), 200
+
+    
+    
+    
